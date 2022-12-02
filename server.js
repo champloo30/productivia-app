737bcee3492b6c41041c2fa84a78c94bf2020387
champloo30 <djld30@gmail.com>
2022-12-02 18:01:02 -0500

    didnt work, trying again

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.urlencoded({extended: false}))
-app.use(express.static('public'))
+app.use(express.static('/client/public'))
 app.use(express.json())
 app.use(require('./routes/myTasks'))
 app.use(require('./routes/myNotes'))
