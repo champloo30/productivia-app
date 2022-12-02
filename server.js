67b95c0551dc0ba5454bc2c13e9f8267fdf6d3de
champloo30 <djld30@gmail.com>
2022-12-02 18:11:43 -0500

    need to add path, trying again

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path =  require('path')
 require('dotenv').config({ path: './config.env' })
 const port = process.env.PORT || 5000
 
