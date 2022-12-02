dc1c1bb25e114acd5ec99f17e2f249cc98c2bfe1
champloo30 <djld30@gmail.com>
2022-12-02 17:38:19 -0500

    another hopeful deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config({ path: './config.env' })
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+app.use(require('./server/routes/myTasks'))
+app.use(require('./server/routes/myNotes'))
+
+const dbo = require('./server/db/conn')
+
+app.listen(port, () => {
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
+    })
+    console.log(`Server running on port: ${port}`);
+})
