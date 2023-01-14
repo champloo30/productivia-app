0d7c76e74747baf481b0d2848b5eb21b896a0028
champloo30 <djld30@gmail.com>
2023-01-14 16:29:47 -0500

    fixed bugs

--- client/src/components/Notes/AddForm/AddForm.jsx
+++ client/src/components/Notes/AddForm/AddForm.jsx
@@ -23,7 +23,7 @@ export default function Form(props) {
     e.preventDefault()
     const newNote = { ...form, category }
 
-    await fetch(`https://productivia-app.herokuapp.com/note/add`, {
+    await fetch(`https://productivia-app.herokuapp.com/api/note/add`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
